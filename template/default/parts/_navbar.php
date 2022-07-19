8e7625411bbe25960c5121cd8ae42dfea21c6896
ihdyrtg <ihdi.syahputra@gmail.com>
2022-07-19 08:02:49 +0700

    init project

--- template/default/parts/_navbar.php
+++ template/default/parts/_navbar.php
@@ -27,16 +27,15 @@ $main_menus = [
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
-    <a class="navbar-brand inline-flex items-center" href="index.php">
-        <?php
-        if(isset($sysconf['logo_image']) && $sysconf['logo_image'] != '' && file_exists('images/default/'.$sysconf['logo_image'])){
-            echo '<img class="h-10 w-15" src="images/default/'.$sysconf['logo_image'].'">';
-        }
-        elseif (file_exists(__DIR__ . '/../assets/images/logo.png')) {
-            echo '<img class="h-8 w-8" src="'.assets('images/logo-big.png').'">';
-        } else {
-        ?>
-        <!-- <svg
+  <a class="navbar-brand inline-flex items-center" href="index.php">
+    <?php
+    if (isset($sysconf['logo_image']) && $sysconf['logo_image'] != '' && file_exists('images/default/' . $sysconf['logo_image'])) {
+      echo '<img class="h-10 w-15" src="images/default/' . $sysconf['logo_image'] . '">';
+    } elseif (file_exists(__DIR__ . '/../assets/images/logo.png')) {
+      echo '<img class="h-8 w-8" src="' . assets('images/logo-big.png') . '">';
+    } else {
+    ?>
+      <!-- <svg
             class="fill-current text-white inline-block h-8 w-8"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -49,124 +48,129 @@ $main_menus = [
                 L4.6,99.7l0,0l0,0C4.2,99.5,4,99.2,4,98.8l0-62.5l0,0l0-0.1c0-0.4,0.2-0.7,0.5-0.9l20.8-12l33.9,19.6l33.9-19.6l20.6,11.9l0.1,0
                 c0.3,0.2,0.5,0.5,0.6,0.9l0,62.3L114.4,98.8L114.4,98.8z M95.3,68.6v39.4L23.1,66.4V26.9L95.3,68.6z"/>
          </svg> -->
-        <?php } ?>
-        <div class="inline-flex flex-col leading-tight ml-2">
-            <h1 class="text-lg m-0 p-0"><?php echo $sysconf['library_name']; ?></h1>
-            <?php if ($sysconf['template']['classic_library_subname']) : ?>
-            <h2 class="text-sm lead m-0 p-0"><?php echo $sysconf['library_subname']; ?></h2>
-            <?php endif; ?>
-        </div>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <?php } ?>
+    <div class="inline-flex flex-col leading-tight ml-2">
+      <h1 class="text-lg m-0 p-0"><?php echo $sysconf['library_name']; ?></h1>
+      <?php if ($sysconf['template']['classic_library_subname']) : ?>
+        <h2 class="text-sm lead m-0 p-0"><?php echo $sysconf['library_subname']; ?></h2>
+      <?php endif; ?>
+    </div>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <?php
-          foreach ($main_menus as $key => $main_menu) {
-            $active = '';
-            if (isset($_GET['p'])) {
-              if ($key === $_GET['p']) $active = 'active';
-            } elseif ($key === 'home') {
-              $active = 'active';
-            }
-            $menu_str = <<<HTML
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <?php
+      foreach ($main_menus as $key => $main_menu) {
+        $active = '';
+        if (isset($_GET['p'])) {
+          if ($key === $_GET['p']) $active = 'active';
+        } elseif ($key === 'home') {
+          $active = 'active';
+        }
+        $menu_str = <<<HTML
 <li class="nav-item {$active}">
     <a class="nav-link" href="{$main_menu['url']}">{$main_menu['text']}</a>
 </li>
 HTML;
-            echo $menu_str;
-          }
-          ?>
-          <?php
-          $menu_member_active = isset($_GET['p']) && $_GET['p'] === 'member' ? 'active' : '';
-          if ($is_login) {
+        echo $menu_str;
+      }
+      ?>
+      <?php
+      $menu_member_active = isset($_GET['p']) && $_GET['p'] === 'member' ? 'active' : '';
+      if ($is_login) {
+      ?>
+        <li class="nav-item <?= $menu_member_active; ?>">
+          <a class="nav-link" href="index.php?p=member&sec=title_basket">
+            <i class="fas fa-shopping-basket"></i>
+            <?php
+            $count_basket = count($_SESSION['m_mark_biblio']);
             ?>
-              <li class="nav-item <?= $menu_member_active; ?>">
-                  <a class="nav-link" href="index.php?p=member&sec=title_basket">
-                      <i class="fas fa-shopping-basket"></i>
-                    <?php
-                    $count_basket = count($_SESSION['m_mark_biblio']);
-                    ?>
-                      <sup id="count-basket" class="badge badge-danger"><?php echo $count_basket; ?></sup>
-                  </a>
-              </li>
-              <li class="nav-item dropdown <?= $menu_member_active; ?>">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                     aria-haspopup="true" aria-expanded="false">
-                      <img class="w-6 h-6 rounded-full ml-2 mr-2"
-                           src="<?php echo $member_image_path; ?>"
-                           alt="Avatar of Jonathan Reinink">
-                    <?php echo $_SESSION['m_name']; ?>
-                  </a>
-                  <div class="dropdown-menu dropdown-menu-right">
-                      <a class="dropdown-item" href="index.php?p=member"><i class="fas fa-user-circle mr-3"></i> <?= __('Profile');?></a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="index.php?p=member&logout=1"><i
-                                  class="fas fa-sign-out-alt mr-3"></i> <?= __('Logout'); ?></a>
-                  </div>
-              </li>
-          <?php } else { ?>
-            <li class="nav-item dropdown">              
-                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-                e-Resource
-                </a>
-              <div class="dropdown-menu bg-blue-lighter dropdown-menu-lg-right" aria-labelledby="dropdownbutton">
-                <a class="dropdown-item" href="https://kubuku.id/download/e-pustaka-iain-padangsidimpuan/">E-Pustaka</a>
-                <a class="dropdown-item" href="http://ebooks.iain-padangsidimpuan.ac.id/index.php">Galeri Buku Digital</a>
-                <a class="dropdown-item" href="http://jurnal.iain-padangsidimpuan.ac.id/">Rumah Jurnal</a>
-                <a class="dropdown-item" href="http://repo.iain-padangsidimpuan.ac.id/">Resource Repository</a>
-                <a class="dropdown-item" href="http://etd.iain-padangsidimpuan.ac.id/">E-Thesis</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">              
-                  <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-                  Login
-                  </a>
+            <sup id="count-basket" class="badge badge-danger"><?php echo $count_basket; ?></sup>
+          </a>
+        </li>
+        <li class="nav-item dropdown <?= $menu_member_active; ?>">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img class="w-6 h-6 rounded-full ml-2 mr-2" src="<?php echo $member_image_path; ?>" alt="Avatar of Jonathan Reinink">
+            <?php echo $_SESSION['m_name']; ?>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="index.php?p=member"><i class="fas fa-user-circle mr-3"></i> <?= __('Profile'); ?></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="index.php?p=member&logout=1"><i class="fas fa-sign-out-alt mr-3"></i> <?= __('Logout'); ?></a>
+          </div>
+        </li>
+      <?php } else { ?>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+            Layanan Online
+          </a>
+          <div class="dropdown-menu bg-blue-lighter dropdown-menu-lg-right" aria-labelledby="dropdownbutton">
+            <a class="dropdown-item" href="index.php?p=perpanjang">Perpanjang Mandiri</a>
+            <a class="dropdown-item" href="index.php?p=member">Bebas Pustaka</a>
+            <a class="dropdown-item" href="index.php?p=member">Cek Peminjaman</a>
+            <a class="dropdown-item" href="index.php?p=member">Cek Status Keanggotaan</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+            e-Resource
+          </a>
+          <div class="dropdown-menu bg-blue-lighter dropdown-menu-lg-right" aria-labelledby="dropdownbutton">
+            <a class="dropdown-item" href="https://kubuku.id/download/e-pustaka-iain-padangsidimpuan/">E-Pustaka</a>
+            <a class="dropdown-item" href="http://ebooks.iain-padangsidimpuan.ac.id/index.php">Galeri Buku Digital</a>
+            <a class="dropdown-item" href="http://jurnal.iain-padangsidimpuan.ac.id/">Rumah Jurnal</a>
+            <a class="dropdown-item" href="http://repo.iain-padangsidimpuan.ac.id/">Resource Repository</a>
+            <a class="dropdown-item" href="http://etd.iain-padangsidimpuan.ac.id/">E-Thesis</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+            Login
+          </a>
           <div class="dropdown-menu bg-blue-lighter dropdown-menu-lg-right" aria-labelledby="dropdownbutton">
             <a class="dropdown-item" href="index.php?p=member&sec=title_basket">Anggota</a>
             <a class="dropdown-item" href="index.php?p=login">Pustakawan</a>
             <a class="dropdown-item" href="index.php?p=daftar_online">Daftar Online</a>
             <a class="dropdown-item" href="index.php?p=visitor">Daftar Pengunjung</a>
           </div>
-              </li>
-          <?php } ?>
-            <li class="nav-item dropdown">
-              <?php
-              $langstr = '';
-              $current_lang = '';
-              $select_lang = isset($_COOKIE['select_lang'])?$_COOKIE['select_lang']:$sysconf['default_lang'];
-              require_once(LANG . 'localisation.php');
-              foreach ($available_languages AS $lang_index) {
-                $selected = null;
-                $lang_code = $lang_index[0];
-                $lang_name = $lang_index[1];
-                $code_arr = explode('_', $lang_code);
-                $code_flag = strtolower($code_arr[1]);
-                if ($lang_code == $select_lang) {
-                  $current_lang = [
-                    'name' => $lang_name,
-                    'code' => $code_flag
-                  ];
-                }
-                $langstr .= <<<HTML
+        </li>
+      <?php } ?>
+      <li class="nav-item dropdown">
+        <?php
+        $langstr = '';
+        $current_lang = '';
+        $select_lang = isset($_COOKIE['select_lang']) ? $_COOKIE['select_lang'] : $sysconf['default_lang'];
+        require_once(LANG . 'localisation.php');
+        foreach ($available_languages as $lang_index) {
+          $selected = null;
+          $lang_code = $lang_index[0];
+          $lang_name = $lang_index[1];
+          $code_arr = explode('_', $lang_code);
+          $code_flag = strtolower($code_arr[1]);
+          if ($lang_code == $select_lang) {
+            $current_lang = [
+              'name' => $lang_name,
+              'code' => $code_flag
+            ];
+          }
+          $langstr .= <<<HTML
     <a class="dropdown-item" href="index.php?select_lang={$lang_code}">
         <span class="flag-icon flag-icon-{$code_flag} mr-2" style="border-radius: 2px;"></span> {$lang_name}
     </a>
 HTML;
-              }
-              ?>
-                <a class="nav-link dropdown-toggle cursor-pointer" type="button" id="languageMenuButton"
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="flag-icon flag-icon-<?= $current_lang['code'] ?>" style="border-radius: 2px;"></span>
-                </a>
-                <div class="dropdown-menu bg-grey-lighter dropdown-menu-lg-right" aria-labelledby="dropdownMenuButton">
-                    <h6 class="dropdown-header"><?= __('Select Language'); ?> : </h6>
-                  <?= $langstr; ?>
-                </div>
-            </li>
-        </ul>
-    </div>
+        }
+        ?>
+        <a class="nav-link dropdown-toggle cursor-pointer" type="button" id="languageMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span class="flag-icon flag-icon-<?= $current_lang['code'] ?>" style="border-radius: 2px;"></span>
+        </a>
+        <div class="dropdown-menu bg-grey-lighter dropdown-menu-lg-right" aria-labelledby="dropdownMenuButton">
+          <h6 class="dropdown-header"><?= __('Select Language'); ?> : </h6>
+          <?= $langstr; ?>
+        </div>
+      </li>
+    </ul>
+  </div>
 </nav>
--- template/default/parts/footer.php
+++ template/default/parts/footer.php
@@ -8,37 +8,43 @@
 ?>
 
 
-<footer class="py-2 bg-secondary text-grey-lighter">
+<footer class="py-2 bg-dark text-grey-lighter">
     <div class="container">
         <div class="row py-2">
-            <div class="col-md-3">
-                <!--
-				<svg
-                        class="fill-current text-grey-lighter block h-12 w-12 mb-2"
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                        viewBox="0 0 118.4 135" style="enable-background:new 0 0 118.4 135;"
-                        xml:space="preserve">
+            <div class="col-md-2">
+
+                <!-- <svg class="fill-current text-grey-lighter block h-12 w-12 mb-2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 118.4 135" style="enable-background:new 0 0 118.4 135;" xml:space="preserve">
                     <path d="M118.3,98.3l0-62.3l0-0.2c-0.1-1.6-1-3-2.3-3.9c-0.1,0-0.1-0.1-0.2-0.1L61.9,0.8c-1.7-1-3.9-1-5.4-0.1l-54,31.1
                     l-0.4,0.2C0.9,33,0.1,34.4,0,36c0,0.1,0,0.2,0,0.3l0,62.4l0,0.3c0.1,1.6,1,3,2.3,3.9c0.1,0.1,0.2,0.1,0.2,0.2l53.9,31.1l0.3,0.2
                     c0.8,0.4,1.6,0.6,2.4,0.6c0.8,0,1.5-0.2,2.2-0.5l53.9-31.1c0.3-0.1,0.6-0.3,0.9-0.5c1.2-0.9,2-2.3,2.1-3.7c0-0.1,0-0.3,0-0.4
                     C118.4,98.6,118.3,98.5,118.3,98.3z M114.4,98.8c0,0.3-0.2,0.7-0.5,0.9c-0.1,0.1-0.2,0.1-0.2,0.1l-20.6,11.9L59.2,92.1l-33.9,19.6
                     L4.6,99.7l0,0l0,0C4.2,99.5,4,99.2,4,98.8l0-62.5l0,0l0-0.1c0-0.4,0.2-0.7,0.5-0.9l20.8-12l33.9,19.6l33.9-19.6l20.6,11.9l0.1,0
-                    c0.3,0.2,0.5,0.5,0.6,0.9l0,62.3L114.4,98.8L114.4,98.8z M95.3,68.6v39.4L23.1,66.4V26.9L95.3,68.6z"/>
-                </svg>
-				-->
-                <img class="s-logo animated flipInY delay7" src="template/default/assets/images/logo_iain.png" width="150" height="150">
-                <div class="mb-4">
-                    Perpustakaan IAIN<br>
-                    Padangsidimpuan </div>
-                <h3><u>Layanan Online</u></h3>
+                    c0.3,0.2,0.5,0.5,0.6,0.9l0,62.3L114.4,98.8L114.4,98.8z M95.3,68.6v39.4L23.1,66.4V26.9L95.3,68.6z" />
+                </svg> -->
+
+                <img class="aligncenter wp-image-2599" src="template/default/assets/images/logo_uin.png" width="200" height="180" style="margin: 2px">
+                <!-- <div>
+                    <i class="fa fa-map-marker"></i> Jalan T. Rizal Nurdin, Km. 4,5 Sihitang. Padangsidimpuan, <br>Kode Pos 22733.
+                    <br>
+                    <i class="fa fa-phone"></i> Telp. (0634) 22080
+                    <br>
+                    <i class="fa fa-fax"></i> Fax. (0634) 24022
+                    <br>
+                    <i class="fa fa-envelope"></i> <a href="humas@iain-padangsidimpuan.ac.id">
+                        <p>
+                            humas@iain-padangsidimpuan.ac.id
+                        </p>
+                    </a>
+                </div> -->
+                <!-- <div class="mb-4">  </div> -->
+                <!-- <h3><u>Layanan Online</u></h3>
                 <a title="Perpanjang Masa Peminjaman Online" class="btn btn-outline-success mb-2" href="index.php?p=perpanjang"><i class="fas fa-calendar-plus mr-2"></i>Perpanjang Mandiri</a>
                 <a title="Bebas Pustaka" class="btn btn-outline-success mb-2" href="index.php?p=member"><i class="fas fa-graduation-cap mr-2"></i>Bebas Pustaka</a>
                 <a title="Cek Peminjaman" class="btn btn-outline-success mb-2" href="index.php?p=member"><i class="fas fa-book mr-2"></i>Cek Peminjaman</a>
                 <a target="_blank" title="Cek Status Keanggotaan" class="btn btn-outline-success mb-2" href="index.php?p=member&sec=my_account"><i class="fas fa-user mr-2"></i>Cek Status Keanggotaan</a>
-                <a target="_blank" title="Perpanjang Masa Peminjaman Online" class="btn btn-outline-success mb-2" href="http://library.fis.uny.ac.id/sp/subjects/index.php"><i class="fas fa-question mr-2"></i>Subject Guide (Topic Corner)</a>
-                <a title="Perpanjang Masa Peminjaman Online" class="btn btn-outline-success mb-2" href="index.php?p=member"><i class="fas fa-file-pdf mr-2"></i>Cek Plagiarisme Tulisan</a>
-                <a target="_blank" title="Perpanjang Masa Peminjaman Online" class="btn btn-outline-success mb-2" href="http://library.fis.uny.ac.id/survei/index.php/298819?lang=id"><i class="fas fa-server mr-2"></i>Survei</a>
+                <a target="_blank" title="Rumah Jurnal" class="btn btn-outline-success mb-2" href="http://jurnal.iain-padangsidimpuan.ac.id/"><i class="fas fa-question mr-2"></i>Subject Guide (Topic Corner)</a>
+                <a title="Cek Plagiarsm" class="btn btn-outline-success mb-2" href="index.php?p=member"><i class="fas fa-file-pdf mr-2"></i>Cek Plagiarisme Tulisan</a>
+                <a target="_blank" title="Perpanjang Masa Peminjaman Online" class="btn btn-outline-success mb-2" href="http://library.fis.uny.ac.id/survei/index.php/298819?lang=id"><i class="fas fa-server mr-2"></i>Survei</a> -->
             </div>
             <div class="col-md-5 pt-8 md:pt-0">
                 <h4 class="mb-4"><?= __('About Us'); ?></h4>
@@ -58,9 +64,9 @@
                         </div>
                     </div>
                 </form>
-                <hr>
+                <!-- <hr>
                 <a target="_blank" title="Support Us" class="btn btn-outline-success mb-2" href="https://slims.web.id/web/pages/support-us/"><i class="fas fa-heart mr-2"></i><?= __('Keep SLiMS Alive'); ?></a>
-                <a target="_blank" title="Contribute" class="btn btn-outline-light mb-2" href="https://github.com/slims/"><i class="fab fa-github mr-2"></i><?= __('Want to Contribute?'); ?></a>
+                <a target="_blank" title="Contribute" class="btn btn-outline-light mb-2" href="https://github.com/slims/"><i class="fab fa-github mr-2"></i><?= __('Want to Contribute?'); ?></a> -->
             </div>
         </div>
         <hr>
